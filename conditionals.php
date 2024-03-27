efee3939dddd4f9cef26d7f226f5cd3cf71a9798
Anna Hill <Anna.Hill@students.jmcss.org>
2024-03-27 08:42:07 -0500

    ww

---
+++ conditionals.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$x = 3;
+
+if ($x == 1) {
+    echo "Anna is very pretty!";
+} 
+elseif($x == 2) {
+    echo "Anna is really great!";
+}
+elseif($x == 3) {
+    echo "Anna is kinda cool!";
+}
+elseif($x == 4) {
+    echo "Anna is awesome!";
+}
+else {
+    echo "Anna is very weird!";
+}
+
+?>
+</body>
+</html>
