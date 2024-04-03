0bb03906836e70c23cd4eb524d60ee1c486d1050
Anna Hill <Anna.Hill@students.jmcss.org>
2024-04-03 07:34:52 -0500

    w

--- functions.php
+++ functions.php
@@ -11,6 +11,19 @@
 
 echo str_replace("Anna","Jenna","Hi Anna"); 
 
+echo "<br>";
+//user defined functions
+$x = 100;
+
+    function newCalc($x) {
+        $newnr = $x * 0.75;
+        echo "here is 75% of what i wrote: ".$newnr;
+    }
+ $x = 100;
+newCalc($x);
+echo "<br>";
+$a = 10;
+newCalc($a);
 ?>   
 </body>
 </html>
